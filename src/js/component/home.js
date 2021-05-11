2bdd943fe82c0c69964540de55a38477eea2417f
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-11 15:55:21 +0000

    falta el set

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import TaskBoard from "./taskboard.jsx";
 
+//create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<TaskBoard />
+			<div className="blog-card spring-fever">
+				<div className="title-content">
+					<h3> ToDo List</h3>
+					<hr />
+					<div className="intro">
+						<TaskBoard />
+					</div>
+				</div>
+				<div className="card-info"></div>
+				<div className="utility-info"></div>
+			</div>
 		</div>
 	);
 }
--- src/js/component/taskboard.jsx
+++ src/js/component/taskboard.jsx
@@ -1,14 +1,45 @@
 import React, { useState, Fragment, useEffect } from "react";
-import Button from "react-bootstrap/Button";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const TaskBoard = () => {
 	const [listTask, setListTask] = useState([]);
 	const [newTask, setNewTask] = useState("");
 	const [MyBoolean, setMyBoolean] = useState(false);
-
+	const [totalUsers, setTotalUsers] = useState([]);
+	const mainURL = "https://assets.breatheco.de/apis/fake/todos/user";
+	//--------------------------------------------------------
 	useEffect(() => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/stefanobello", {
+		fetch(mainURL, {
+			method: "GET",
+			//   body: JSON.stringify(todos),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				if (!response.ok) {
+					console.log(response.statusText);
+					throw Error(response.statusText);
+				}
+				console.log(response.ok); // will be true if the response is successfull
+				console.log(response.status); // the status code = 200 or code = 400 etc.
+
+				// will try return the exact result as string
+				return response.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				setTotalUsers(data);
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	}, []);
+	//------------------------------------------------------
+	useEffect(() => {
+		fetch(mainURL.concat(event.target.value), {
 			method: "GET",
 			//   body: JSON.stringify(todos),
 			headers: {
@@ -61,7 +92,7 @@ const TaskBoard = () => {
 	};
 	//--------------------------------------------------------------------------------------------
 	useEffect(() => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/stefanobello", {
+		fetch(mainURL.concat(event.target.value), {
 			method: "PUT",
 			body: JSON.stringify(listTask),
 			headers: {
@@ -86,6 +117,18 @@ const TaskBoard = () => {
 	//-------------------------------------------------------------------------------------------------------------------------------
 	return (
 		<Fragment>
+			<input
+				type="text"
+				placeholder="User Name"
+				onKeyDown={event => {
+					if (event.key === "Enter") {
+						let found = totalUsers.find(
+							element => element === event.target.value
+						);
+						if (found === event.target.value)
+							return mainURL.concat(event.target.value);
+					}
+				}}></input>
 			<input
 				type="text"
 				placeholder="my new task"
